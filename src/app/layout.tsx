ba80405049aeeedc29bc49af15be999048a8a819
Jomagene <semjomagene@gmail.com>
2024-09-10 11:00:26 +0200

    Implement starter

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
