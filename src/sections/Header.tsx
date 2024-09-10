90519ee074724c84d96039653feb7ff752f21414
Jomagene <semjomagene@gmail.com>
2024-09-10 16:04:02 +0200

    Implement header section style and content

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,21 @@
 export const Header = () => {
   return (
-    <div>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Projects</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border-white/15 border bg-white/10 backdrop-blur rounded-full">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a
+          href="#"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+          Contact
+        </a>
       </nav>
     </div>
   );
