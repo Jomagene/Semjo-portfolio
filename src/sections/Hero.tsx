f9b7efbff68a00ac2848d90ee19cceacc8d9c19b
Jomagene <semjomagene@gmail.com>
2024-09-10 19:01:25 +0200

    Implement the hero-section layout and content

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from "@/assets/icons/arrow-down.svg";
 import Arrow from "@/assets/icons/arrow-down.svg";
 
 export const HeroSection = () => (
-  <div className="py-32">
+  <div className="py-32 md:py-48 lg:py-60">
     <div className="container">
       <div className="flex flex-col items-center">
         <Image
@@ -17,19 +17,23 @@ export const HeroSection = () => (
           <div className="text-sm font-medium">Available for new projects</div>
         </div>
       </div>
-      <h1>Building Exceptional User Experiences</h1>
-      <p>
-        I specialize in transforming designs into functional, hight-performing
-        web applications. Let's discuss your next project.
-      </p>
-      <div>
-        <button>
-          <span>Explore my work</span>
-          <ArrowDown />
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center my-8 tracking-wide">
+          Building Exceptional User Experiences
+        </h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">
+          I specialize in transforming designs into functional, hight-performing
+          web applications. Let's discuss your next project.
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Explore my work</span>
+          <ArrowDown className="size-4" />
         </button>
-        <button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
           <span>👋</span>
-          <span>Let's connect</span>
+          <span className="font-semibold">Let's connect</span>
         </button>
       </div>
     </div>
