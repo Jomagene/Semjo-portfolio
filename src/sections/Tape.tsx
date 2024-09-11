5b4e51a3b663150f058964bdc60fd1d4ac3799e5
Jomagene <semjomagene@gmail.com>
2024-09-11 13:26:16 +0200

    Add a mask-image and rotations

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -14,16 +14,16 @@ const qualities = [
 
 export const TapeSection = () => {
   return (
-    <div className="py-16">
-      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip">
-        <div className="flex">
+    <div className="py-16 lg:py-24  overflow-x-clip">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-4 py-3">
             {qualities.map((quality) => (
               <div key={quality} className="inline-flex gap-4 items-center">
                 <span className="text-gray-900 uppercase font-extrabold text-sm">
                   {quality}
                 </span>
-                <StarIcon className="size-6 text-gray-900" />
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
               </div>
             ))}
           </div>
