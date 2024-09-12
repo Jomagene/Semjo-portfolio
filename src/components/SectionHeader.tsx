d55db4178fd2f6b6cf8f48a0b5d532c0b64ef345
Jomagene <semjomagene@gmail.com>
2024-09-12 20:40:28 +0200

    Create a section header

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const SectionHeader = ({
+  title,
+  eyebrow,
+  description,
+}: {
+  title: String;
+  eyebrow: string;
+  description: string;
+}) => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-4000 text-transparent bg-clip-text">
+          {eyebrow}
+        </p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+        {title}
+      </h2>
+      <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
+        {description}
+      </p>
+    </>
+  );
+};
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,6 +3,7 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
 
 const testimonials = [
@@ -41,8 +42,11 @@ const testimonials = [
 export const TestimonialsSection = () => {
   return (
     <div>
-      <p>Happy client</p>
-      <h2>What Clients Say About Me</h2>
+      <SectionHeader
+        eyebrow="Happy client"
+        title="What Clients Say About Me"
+        description="Don't just take my word for it. See what clients say about my Work"
+      />
       <div>
         {testimonials.map((testimonial) => (
           <div key={testimonial.name}>
