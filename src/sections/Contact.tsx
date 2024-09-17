679ac44fd5d102b60b58501a3db265e6c791e988
Jomagene <semjomagene@gmail.com>
2024-09-17 16:55:40 +0200

    Fix contact section desktop and laptop layout

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,18 +1,24 @@
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
 export const ContactSection = () => {
   return (
-    <div className="py-16 pt-14">
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
-          <h2 className="font-serif text-2xl">
-            Let's create something amazing together
-          </h2>
-          <p className="text-sm mt-2">
-            Ready to bring your next project to life? Let's connect and discuss
-            how I can help you achieve goals
-          </p>
-          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8">
-            <span className="font-semibold">Contact Me</span>{" "}
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0 md:flex md:gap-8">
+          <div
+            className="absolute inset-0 -z-10 opacity-5"
+            style={{ backgroundImage: `url(${grainImage.src})` }}></div>
+          <div>
+            <h2 className="font-serif text-2xl md:text-3xl">
+              Let's create something amazing together
+            </h2>
+            <p className="text-sm md:text-base mt-2">
+              Ready to bring your next project to life? Let's connect and
+              discuss how I can help you achieve goals
+            </p>
+          </div>
+          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 border border-gray-900">
+            <span className="font-semibold w-max">Contact Me</span>
             <ArrowUpRightIcon className="size-4" />
           </button>
         </div>
