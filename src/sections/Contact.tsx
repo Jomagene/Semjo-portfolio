5b93c1dc9073d26fc1f266d36e36b9e84e3448a9
Jomagene <semjomagene@gmail.com>
2024-09-17 15:44:37 +0200

    Add a contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,22 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-14">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center">
+          <h2 className="font-serif text-2xl">
+            Let's create something amazing together
+          </h2>
+          <p className="text-sm mt-2">
+            Ready to bring your next project to life? Let's connect and discuss
+            how I can help you achieve goals
+          </p>
+          <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8">
+            <span className="font-semibold">Contact Me</span>{" "}
+            <ArrowUpRightIcon className="size-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
