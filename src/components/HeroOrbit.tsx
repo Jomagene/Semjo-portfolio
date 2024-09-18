ce2bb9a8f3338495823cc041e09de4bd47e803fe
Jomagene <semjomagene@gmail.com>
2024-09-18 16:45:44 +0200

    Improve the reusability of HeroOrbit component

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -4,20 +4,39 @@ export const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  shouldOrbit = false,
+  shouldSpin = false,
+  orbitDuration,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  orbitDuration?: string;
+  spinDuration?: string;
+}>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-        className="flex items-start justify-start"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          height: `${size}px`,
-          width: `${size}px`,
-        }}>
+        className={shouldOrbit ? "animate-spin" : ""}
+        style={{ animationDuration: orbitDuration }}>
         <div
-          className="inline-flex"
-          style={{ transform: `rotate(${rotation * -1}deg)` }}>
-          {children}
+          className="flex items-start justify-start"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}>
+          <div
+            className={shouldSpin ? "animate-spin" : ""}
+            style={{ animationDuration: spinDuration }}>
+            <div
+              className="inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}>
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -57,7 +57,9 @@ export const HeroSection = () => (
           className="size-[100px]"
         />
         <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="relative size-2.5 bg-green-500 rounded-full">
+            <span className="absolute inset-0 bg-green-500/90 animate-ping-large rounded-full"></span>
+          </div>
           <div className="text-sm font-medium">Available for new projects</div>
         </div>
       </div>
