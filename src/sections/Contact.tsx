0d7fb8ddf70b0bccd070562bb6e04a91440671b0
Jomagene <semjomagene@gmail.com>
2024-09-18 09:30:37 +0200

    Feat footer layout and responsiveness

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,7 +4,7 @@ export const ContactSection = () => {
   return (
     <div className="py-16 pt-12 lg:py-24 lg:pt-20">
       <div className="container">
-        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0 md:flex md:gap-8">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0 md:flex md:justify-between items-center md:gap-8">
           <div
             className="absolute inset-0 -z-10 opacity-5"
             style={{ backgroundImage: `url(${grainImage.src})` }}></div>
@@ -12,7 +12,7 @@ export const ContactSection = () => {
             <h2 className="font-serif text-2xl md:text-3xl">
               Let&#44;s create something amazing together
             </h2>
-            <p className="text-sm md:text-base mt-2">
+            <p className="text-sm md:text-base my-2">
               Ready to bring your next project to life? Let&#44;s connect and
               discuss how I can help you achieve goals
             </p>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const footerLinks = [
@@ -13,16 +12,20 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
-        <div>
-          <div>&copy; 2024. All rights reserved.</div>
-          <nav>
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+          <div className="text-white/40">&copy; 2024. All rights reserved.</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8 ">
             {footerLinks.map((link) => (
-              <Link key={link.title} href={link.href}>
-                <span>{link.title}</span>
-                <ArrowUpRightIcon />
-              </Link>
+              <a
+                key={link.title}
+                href={link.href}
+                className="inline-flex items-center gap-1.5 cursor-pointer">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
             ))}
           </nav>
         </div>
