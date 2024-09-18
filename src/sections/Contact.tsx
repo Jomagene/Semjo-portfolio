d3b4043e003ae0654f5514a2992905a0ee02b93f
Jomagene <semjomagene@gmail.com>
2024-09-18 06:16:07 +0200

    Add a footer content

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -10,10 +10,10 @@ export const ContactSection = () => {
             style={{ backgroundImage: `url(${grainImage.src})` }}></div>
           <div>
             <h2 className="font-serif text-2xl md:text-3xl">
-              Let's create something amazing together
+              Let&#44;s create something amazing together
             </h2>
             <p className="text-sm md:text-base mt-2">
-              Ready to bring your next project to life? Let's connect and
+              Ready to bring your next project to life? Let&#44;s connect and
               discuss how I can help you achieve goals
             </p>
           </div>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  { title: "Github", href: "https://github.com/Jomagene" },
+  { title: "Twitter", href: "https://x.com/jomagene" },
+  {
+    title: "Frontendmentor",
+    href: "https://www.frontendmentor.io/profile/Jomagene",
+  },
+  { title: "LinkedIn", href: "https://www.linkedin.com/in/jo%C3%ABl-magene/" },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <div>&copy; 2024. All rights reserved.</div>
+          <nav>
+            {footerLinks.map((link) => (
+              <Link key={link.title} href={link.href}>
+                <span>{link.title}</span>
+                <ArrowUpRightIcon />
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
