5218d4eb50bc1834f4de8a6c68456697d824f609
Jomagene <semjomagene@gmail.com>
2025-04-29 07:07:55 +0200

    refactor: hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,9 +3,8 @@ import Image from 'next/image';
 import memojiImage from '@/assets/images/memoji-smile.jpg';
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg';
-import StarIcon from '@/assets/icons/star.svg';
-import SparkleIcon from '@/assets/icons/sparkle.svg';
 import { HeroOrbit } from '@/components/HeroOrbit';
+import { orbits } from '@/data/hero-orbits';
 import { motion } from 'framer-motion';
 
 export const HeroSection = () => {
@@ -22,78 +21,18 @@ export const HeroSection = () => {
         <div className="size-[1020px] hero-ring"></div>
         <div className="size-[1220px] hero-ring"></div>
 
-        <HeroOrbit
-          size={430}
-          rotation={-14}
-          shouldOrbit
-          shouldSpin
-          orbitDuration="30s"
-          spinDuration="3s">
-          <SparkleIcon className="size-8 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={440}
-          rotation={79}
-          shouldOrbit
-          orbitDuration="32s"
-          shouldSpin
-          spinDuration="3s">
-          <SparkleIcon className="size-5 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
-          <div className="size-2 rounded-full bg-emerald-300/20"></div>
-        </HeroOrbit>
-        <HeroOrbit
-          size={530}
-          rotation={178}
-          shouldOrbit
-          orbitDuration="36s"
-          shouldSpin
-          spinDuration="3s">
-          <SparkleIcon className="size-10 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={550}
-          rotation={20}
-          shouldOrbit
-          shouldSpin
-          orbitDuration="38s"
-          spinDuration="6s">
-          <StarIcon className="size-12 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={590}
-          rotation={98}
-          shouldOrbit
-          shouldSpin
-          orbitDuration="40s"
-          spinDuration="6s">
-          <StarIcon className="size-8 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
-          <div className="size-2 rounded-full bg-emerald-300/20"></div>
-        </HeroOrbit>
-        <HeroOrbit
-          size={710}
-          rotation={144}
-          shouldOrbit
-          orbitDuration="44s"
-          shouldSpin
-          spinDuration="6s">
-          <SparkleIcon className="size-14 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
-          <div className="size-3 rounded-full bg-emerald-300/20"></div>
-        </HeroOrbit>
-        <HeroOrbit
-          size={800}
-          rotation={-72}
-          shouldOrbit
-          shouldSpin
-          orbitDuration="48s"
-          spinDuration="6s">
-          <StarIcon className="size-28 text-emerald-300" />
-        </HeroOrbit>
+        {orbits.map((orbit, index) => (
+          <HeroOrbit
+            key={index}
+            size={orbit.size}
+            rotation={orbit.rotation}
+            shouldOrbit={orbit.shouldOrbit}
+            shouldSpin={orbit.shouldSpin}
+            orbitDuration={orbit.orbitDuration}
+            spinDuration={orbit.spinDuration}>
+            {orbit.content}
+          </HeroOrbit>
+        ))}
       </div>
 
       <div className="container relative pb-5">
